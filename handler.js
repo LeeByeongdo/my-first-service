0dcb365184fa9d4ba20f2d8631d44e7d70023f4d
Byeongdo Lee <bdbdbdlee@gmail.com>
2020-03-16 22:57:05 +0900

    기본설정완료

--- handler.js
+++ handler.js
@@ -1,25 +1,66 @@
 "use strict";
-const { tagEvent } = require("./serverless_sdk");
+const jwt = require("jsonwebtoken");
+const sha256 = require("js-sha256").sha256;
 
-module.exports.hello = async event => {
-  tagEvent("custom-tag", "hello world", { custom: { tag: "data" } });
+module.exports.hello = async (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
-  };
+  if (headers["device"]) {
+    const device = headers["device"][0].value;
+    if (device === "mobile") {
+      headers["isMobile"] = [{ key: "isMobile", value: "true" }];
+    } else {
+      headers["isMobile"] = [{ key: "isMobile", value: "false" }];
+    }
+  } else {
+    headers["isMobile"] = [{ key: "isMobile", value: "false" }];
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  let isEmployee = false;
+
+  // from csr
+  const authorizationHeader = headers["authorization"]
+    ? headers["authorization"][0].value
+    : "";
+  if (authorizationHeader && authorizationHeader.split(" ").length > 1) {
+    const token = authorizationHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(
+        token,
+        "1234567890123456789012345678901234567890123456789012345678901234"
+      );
+      isEmployee = decoded.isEmployee;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+      isEmployee = false;
+    }
+  } else {
+    // from ssr
+    const sessionHeader = headers["usersession"]
+      ? headers["usersession"][0].value
+      : "";
+    if (sessionHeader && sessionHeader.length > 3) {
+      const checksumKey =
+        "1234567890123456789012345678901234567890123456789012345678901234";
+      const split = sessionHeader.split("|");
+      const sid = split[0];
+      const option = Number(split[1]);
+      const timestamp = split[2];
+      const checksum = split[3];
+
+      if (
+        sha256(`${sid}|${option}|${timestamp}${checksumKey}`).toUpperCase() ===
+        checksum
+      ) {
+        isEmployee = (option & 3) === 3;
+      }
+    }
+  }
+
+  headers["isEmployee"] = [{ key: "isEmployee", value: String(isEmployee) }];
+
+  callback(null, request);
 };
